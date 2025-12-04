05e48c30ca5eb53a865ced9ff2ab7b4320a9433e
Fernando Olivares <folivares@37signals.com>
2025-12-04 12:14:23 -0600

    Allow JSON requests submitting a magic link code

--- app/controllers/sessions/magic_links_controller.rb
+++ app/controllers/sessions/magic_links_controller.rb
@@ -1,7 +1,7 @@
 class Sessions::MagicLinksController < ApplicationController
   disallow_account_scope
   require_unauthenticated_access
-  rate_limit to: 10, within: 15.minutes, only: :create, with: -> { redirect_to session_magic_link_path, alert: "Try again in 15 minutes." }
+  rate_limit to: 10, within: 15.minutes, only: :create, with: :rate_limit_exceeded
 
   layout "public"
 
@@ -10,10 +10,9 @@ class Sessions::MagicLinksController < ApplicationController
 
   def create
     if magic_link = MagicLink.consume(code)
-      start_new_session_for magic_link.identity
-      redirect_to after_sign_in_url(magic_link)
+      respond_to_valid_code_from magic_link
     else
-      redirect_to session_magic_link_path, alert: "Try another code."
+      respond_to_invalid_code
     end
   end
 
@@ -22,11 +21,40 @@ class Sessions::MagicLinksController < ApplicationController
       params.expect(:code)
     end
 
+    def respond_to_valid_code_from(magic_link)
+      respond_to do |format|
+        format.html do
+          start_new_session_for magic_link.identity
+          redirect_to after_sign_in_url(magic_link)
+        end
+
+        format.json do
+          new_access_token = magic_link.identity.access_tokens.create!(permission: :write)
+          render json: { access_token: new_access_token.token }
+        end
+      end
+    end
+
+    def respond_to_invalid_code
+      respond_to do |format|
+        format.html { redirect_to session_magic_link_path, alert: "Try another code." }
+        format.json { render json: { message: "Try another code." }, status: :unauthorized }
+      end
+    end
+
     def after_sign_in_url(magic_link)
       if magic_link.for_sign_up?
         new_signup_completion_path
       else
         after_authentication_url
+      end
+    end
+
+    def rate_limit_exceeded
+      rate_limit_exceeded_message = "Try again later."
+      respond_to do |format|
+        format.html { redirect_to session_magic_link_path, alert: rate_limit_exceeded_message }
+        format.json { render json: { message: rate_limit_exceeded_message }, status: :too_many_requests }
       end
     end
 end
