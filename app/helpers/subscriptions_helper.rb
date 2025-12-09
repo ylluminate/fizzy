7596f2bad074545fc49b4ebd8839e2c99f6fb8a3
Jason Zimdars <jz@37signals.com>
2025-12-09 13:49:20 -0600

    Show a notice when you're out of cards

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -8,4 +8,8 @@ module SubscriptionsHelper
       "Your next payment of <b>$#{ subscription.plan.price }</b> will be billed on".html_safe
     end
   end
+
+  def exceeds_plan_cards_limit
+    Current.account.cards_count >= Plan.free.card_limit
+  end
 end
