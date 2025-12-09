9d5fafa2f71799d531dd72de9b49679d5097c658
Stanko K.R. <stanko@stanko.io>
2025-12-09 11:30:37 +0100

    Add API for columns

--- app/controllers/boards/columns_controller.rb
+++ app/controllers/boards/columns_controller.rb
@@ -6,18 +6,38 @@ class Boards::ColumnsController < ApplicationController
   def show
     set_page_and_extract_portion_from @column.cards.active.latest.with_golden_first.preloaded
     fresh_when etag: @page.records
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
     @column = @board.columns.create!(column_params)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.json { head :created, location: board_column_path(@board, @column, format: :json) }
+    end
   end
 
   def update
     @column.update!(column_params)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.json { head :no_content }
+    end
   end
 
   def destroy
     @column.destroy
+
+    respond_to do |format|
+      format.turbo_stream
+      format.json { head :no_content }
+    end
   end
 
   private
--- test/controllers/boards/columns_controller_test.rb
+++ test/controllers/boards/columns_controller_test.rb
@@ -36,4 +36,43 @@ class Boards::ColumnsControllerTest < ActionDispatch::IntegrationTest
       assert_response :success
     end
   end
+
+  test "show as JSON" do
+    column = columns(:writebook_in_progress)
+
+    get board_column_path(column.board, column), as: :json
+
+    assert_response :success
+    assert_equal column.id, @response.parsed_body["id"]
+  end
+
+  test "create as JSON" do
+    board = boards(:writebook)
+
+    assert_difference -> { board.columns.count }, +1 do
+      post board_columns_path(board), params: { column: { name: "New Column" } }, as: :json
+    end
+
+    assert_response :created
+    assert_equal board_column_path(board, Column.last, format: :json), @response.headers["Location"]
+  end
+
+  test "update as JSON" do
+    column = columns(:writebook_in_progress)
+
+    put board_column_path(column.board, column), params: { column: { name: "Updated Name" } }, as: :json
+
+    assert_response :no_content
+    assert_equal "Updated Name", column.reload.name
+  end
+
+  test "destroy as JSON" do
+    column = columns(:writebook_on_hold)
+
+    assert_difference -> { column.board.columns.count }, -1 do
+      delete board_column_path(column.board, column), as: :json
+    end
+
+    assert_response :no_content
+  end
 end
