13cea628607bb3fabd73088d8462412e369bc448
ylluminate <ylluminate@gmail.com>
2025-12-11 02:05:24 -0500

    Disable force_ssl for LAN deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
-  config.assume_ssl = ENV.fetch("ASSUME_SSL", "true") == "true"
+  config.assume_ssl = ENV.fetch("ASSUME_SSL", "true") == "false"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = ENV.fetch("FORCE_SSL", "true") == "false"
