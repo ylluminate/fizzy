81e07e5fc4a3af251107f2103ba7c67564417f69
ylluminate <ylluminate@gmail.com>
2025-12-15 14:12:24 -0500

    Add debug logging to reverse proxy URL options

--- config/initializers/reverse_proxy_url_options.rb
+++ config/initializers/reverse_proxy_url_options.rb
@@ -1,38 +1,16 @@
 # Respect X-Forwarded-Host/Proto headers when behind a reverse proxy (NPM, etc.)
-# Uses prepend to avoid modifying upstream files, reducing merge conflicts.
 
 module ReverseProxyUrlOptions
-  extend ActiveSupport::Concern
-
-  included do
-    prepend_before_action :set_reverse_proxy_url_options
-  end
-
-  def set_reverse_proxy_url_options
-    forwarded_host = request.headers["X-Forwarded-Host"].presence
-    forwarded_proto = request.headers["X-Forwarded-Proto"].presence
-
-    # Set Active Storage URL options for blob/variant URL generation
-    if forwarded_host || forwarded_proto
-      ActiveStorage::Current.url_options = {
-        protocol: forwarded_proto ? "#{forwarded_proto}://" : request.protocol,
-        host: forwarded_host || request.host,
-        port: forwarded_host ? nil : request.port,
-        script_name: request.script_name
-      }
-    end
-  end
-
   def default_url_options
     options = super rescue {}
 
-    if (forwarded_host = request.headers["X-Forwarded-Host"].presence)
-      options[:host] = forwarded_host
-    end
+    forwarded_host = request.headers["X-Forwarded-Host"].presence
+    forwarded_proto = request.headers["X-Forwarded-Proto"].presence
 
-    if (forwarded_proto = request.headers["X-Forwarded-Proto"].presence)
-      options[:protocol] = forwarded_proto
-    end
+    Rails.logger.info "ReverseProxyUrlOptions: host=#{forwarded_host.inspect} proto=#{forwarded_proto.inspect}"
+
+    options[:host] = forwarded_host if forwarded_host
+    options[:protocol] = forwarded_proto if forwarded_proto
 
     options
   end
